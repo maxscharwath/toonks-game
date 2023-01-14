98f2b68c195cbb0484f01565e0b4d9a8dd9dc30f
Lazzzer <dev@lazarpavicevic.ch>
2023-01-14 17:59:13 +0100

    wip styling status

--- src/ui/PlayersStatus.tsx
+++ src/ui/PlayersStatus.tsx
@@ -1,10 +1,13 @@
 import type Tank from '@game/models/Tank';
-import TankPlayer from '@game/models/TankPlayer';
+import type TankPlayer from '@game/models/TankPlayer';
+import {TankTypes} from '@game/models/TankType';
+import clsx from 'clsx';
+import {motion} from 'framer-motion';
 import React, {useEffect, useState} from 'react';
 
-function PlayerStatus({tankRaw}: {tankRaw: Tank}) {
+function PlayerStatus({tankRaw, isPlayer}: {tankRaw: Tank; isPlayer: boolean}) {
 	const [tankData, setTankData] = useState({
-		isPlayer: tankRaw instanceof TankPlayer,
+		type: tankRaw.type,
 		pseudo: tankRaw.pseudo,
 		health: tankRaw.health,
 	});
@@ -23,18 +26,32 @@ function PlayerStatus({tankRaw}: {tankRaw: Tank}) {
 	}, [tankRaw]);
 
 	return (
-		<div className='text-xl text-white'>
-			<span>{tankData.pseudo} : </span> <span>{tankData.health}</span>
+		<div className={clsx(!isPlayer && 'scale-75', 'flex space-x-3 text-xl text-white')} onClick={() => {
+			tankRaw.hit(10);
+		}}>
+			<img className='h-24 w-auto rounded-xl border-2 border-gray-900/50 shadow-inner drop-shadow-md 2xl:w-32' src={TankTypes[tankData.type].avatar} />
+			<div className='w-64 space-y-2 bg-gray-200/10'>
+				<motion.div
+					initial={{width: `${tankData.health}%`}}
+					animate={{width: `${tankData.health}%`}}
+					className='h-10 bg-green-300/20'>
+					{tankData.health}
+				</motion.div>
+				<div className='h-10'>
+					{tankData.pseudo}
+				</div>
+			</div>
+
 		</div>
 	);
 }
 
 export default function PlayersStatus({player, tanks}: {player?: TankPlayer; tanks: Tank[]}) {
 	return (
-		<div className='absolute top-0 left-0 z-10 m-4'>
-			{player && <PlayerStatus tankRaw={player} key={player.uuid} />}
+		<div className='absolute top-0 left-0 z-10 m-4 space-y-4'>
+			{player && <PlayerStatus tankRaw={player} isPlayer={true} />}
 			{tanks.map(tank => (
-				<PlayerStatus tankRaw={tank} key={tank.uuid} />
+				<PlayerStatus tankRaw={tank} isPlayer={false} key={tank.uuid} />
 			))}
 		</div>
 	);
